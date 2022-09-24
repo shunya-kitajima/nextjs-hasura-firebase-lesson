e4e9df9c7848604ab54ce4b509f9633b0a3e1bae
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-09-24 21:46:23 +0900

    setting initialization

---
+++ firebaseConfig.ts
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+!firebase.apps.length
+  ? firebase.initializeApp({
+      apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+      authDomain: process.env.NEXT_PUBLIC_FIREBASE_DOMAIN,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
+      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    })
+  : firebase.app()
