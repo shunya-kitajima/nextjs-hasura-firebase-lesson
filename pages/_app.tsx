3c2c4b3b3f90c224838128881cb435f0e38361ac
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-09-26 22:07:01 +0900

    lap queryclient for app component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,33 @@
 import '../styles/globals.css'
+import { useState } from 'react'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import { useUserChanged } from '../hooks/useUserChanged'
 import { store } from '../app/store'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const {} = useUserChanged()
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: false,
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  )
 
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
