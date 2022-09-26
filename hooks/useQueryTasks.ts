32c58caec68966f9a84f4d57f19db27231dd1485
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-09-26 18:37:46 +0900

    modify query

--- hooks/useQueryTasks.ts
+++ hooks/useQueryTasks.ts
@@ -1,24 +1,35 @@
-import { request } from 'graphql-request'
+import { useEffect } from 'react'
+import { GraphQLClient } from 'graphql-request'
 import { useQuery } from 'react-query'
+import Cookie from 'universal-cookie'
 import { Task } from '../types/types'
 import { GET_TASKS } from '../queries/queries'
 
-interface TaskRes {
+const cookie = new Cookie()
+const endpoint = process.env.NEXT_PUBLIC_HASURA_ENDPOINT
+let graphqlClient: GraphQLClient
+
+interface TasksRes {
   tasks: Task[]
 }
 
 export const fetchTasks = async () => {
-  const { tasks: data } = await request<TaskRes>(
-    process.env.NEXT_PUBLIC_HASURA_ENDPOINT,
-    GET_TASKS
-  )
+  const { tasks: data } = await graphqlClient.request<TasksRes>(GET_TASKS)
   return data
 }
 
 export const useQueryTasks = () => {
+  useEffect(() => {
+    graphqlClient = new GraphQLClient(endpoint, {
+      headers: {
+        Authorization: `Bearer ${cookie.get('token')}`,
+      },
+    })
+  }, [cookie.get('token')])
+
   return useQuery<Task[], Error>({
     queryKey: 'tasks',
     queryFn: fetchTasks,
-    staleTime: Infinity,
+    staleTime: 0,
   })
 }
