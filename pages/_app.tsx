07d075efcce9e694fd5289d514227a4ad9d06fab
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-09-26 15:28:23 +0900

    lap by Provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
 import { useUserChanged } from '../hooks/useUserChanged'
+import { store } from '../app/store'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const {} = useUserChanged()
 
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
