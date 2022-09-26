ed3de3cef05de09a20566d766a9a84f86ecc4421
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-09-26 10:31:03 +0900

    add file and create Layout component

---
+++ components/Layout.tsx
@@ -0,0 +1,36 @@
+import React, { ReactNode } from 'react'
+import Head from 'next/head'
+import Image from 'next/image'
+
+interface Props {
+  children: ReactNode
+  title: string
+}
+
+export const Layout: React.FC<Props> = ({
+  children,
+  title = 'Welcom to Nextjs',
+}) => {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen text-gray-600 text-sm font-mono">
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <header></header>
+      <main>{children}</main>
+      <footer className="w-full h-12 flex justify-center items-center border-t">
+        <a
+          className="flex items-center"
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className="h-4 ml-2">
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </div>
+  )
+}
