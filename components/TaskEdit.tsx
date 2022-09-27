b201e8c55fb44191d114f4060d316435b455c81d
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-09-27 17:07:19 +0900

    create form

--- components/TaskEdit.tsx
+++ components/TaskEdit.tsx
@@ -3,6 +3,33 @@ import { useSelector, useDispatch } from 'react-redux'
 import { useAppMutate } from '../hooks/useAppMutate'
 import { setEditedTask, selectTask } from '../slices/uiSlice'
 
-export const TaskEdit = () => {
-  return <div>TaskEdit</div>
+export const TaskEdit: React.FC = () => {
+  const dispatch = useDispatch()
+  const editedTask = useSelector(selectTask)
+  const { createTaskMutation, updateTaskMutation } = useAppMutate()
+
+  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (editedTask.id === '') {
+      createTaskMutation.mutate(editedTask.title)
+    } else {
+      updateTaskMutation.mutate(editedTask)
+    }
+  }
+
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <input
+          className="mb-3 px-3 py-2 border border-gray-300"
+          placeholder="new task ?"
+          type="text"
+          value={editedTask.title}
+          onChange={(e) =>
+            dispatch(setEditedTask({ ...editedTask, title: e.target.value }))
+          }
+        />
+      </form>
+    </div>
+  )
 }
