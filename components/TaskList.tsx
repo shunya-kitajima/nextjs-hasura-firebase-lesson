ebddf14091336200e4c92384db54c3d9d378dd04
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-09-27 16:59:35 +0900

    create TaskList component

---
+++ components/TaskList.tsx
@@ -0,0 +1,24 @@
+import React, { memo } from 'react'
+import { useQueryTasks } from '../hooks/useQueryTasks'
+import { TaskItemMemo } from './TaskItem'
+
+export const TaskList: React.FC = () => {
+  const { status, data } = useQueryTasks()
+
+  if (status === 'loading') return <div>{'Loading...'}</div>
+  if (status === 'error') return <div>{'Error'}</div>
+
+  return (
+    <div>
+      {data?.map((task) => (
+        <div key={task.id}>
+          <ul>
+            <TaskItemMemo task={task} />
+          </ul>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export const TaskListMemo = memo(TaskList)
