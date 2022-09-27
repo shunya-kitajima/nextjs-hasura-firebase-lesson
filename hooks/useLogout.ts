c430c0026c44aec0e04a816bca744bbc8310872d
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-09-27 21:51:37 +0900

    add remove queries and reset state

--- hooks/useLogout.ts
+++ hooks/useLogout.ts
@@ -1,15 +1,25 @@
+import { useQueryClient } from 'react-query'
+import { useDispatch } from 'react-redux'
 import Cookie from 'universal-cookie'
 import firebase from '../firebaseConfig'
 import { unSubMeta } from './useUserChanged'
+import { resetEditedNews, resetEditedTask } from '../slices/uiSlice'
 
 const cookie = new Cookie()
 
 export const useLogout = () => {
+  const queryClient = useQueryClient()
+  const dispatch = useDispatch()
   const logout = async () => {
     if (unSubMeta) {
       unSubMeta()
     }
+    dispatch(resetEditedNews())
+    dispatch(resetEditedTask())
     await firebase.auth().signOut()
+    queryClient.removeQueries('news')
+    queryClient.removeQueries('tasks')
+
     cookie.remove('token')
   }
 
