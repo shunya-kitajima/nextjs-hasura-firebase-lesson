3466dc9698127cfddfd242a3c917c1ccda8d99a0
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-09-27 17:13:12 +0900

    import TaskListMemo and TaskEditaMemo

--- pages/tasks.tsx
+++ pages/tasks.tsx
@@ -10,6 +10,8 @@ import { Layout } from '../components/Layout'
 import firebase from '../firebaseConfig'
 import { NewsListMemo } from '../components/NewsList'
 import { NewsEditMemo } from '../components/NewsEdit'
+import { TaskListMemo } from '../components/TaskList'
+import { TaskEditMemo } from '../components/TaskEdit'
 
 const Tasks: React.FC = () => {
   const router = useRouter()
@@ -30,6 +32,11 @@ const Tasks: React.FC = () => {
       <div className="grid grid-cols-2 gap-40">
         <NewsListMemo />
         <NewsEditMemo />
+      </div>
+      <p className="mt-10 mb-5 text-blue-500 text-xl font-bold">Task Edit</p>
+      <div className="grid grid-cols-2 gap-40">
+        <TaskListMemo />
+        <TaskEditMemo />
       </div>
       <Link href="/">
         <div className="mt-20 flex items-center cursor-pointer">
