ea3dc0db9b2b2e21a15f5d933db91455c9a9f5cd
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-09-28 15:25:39 +0900

    import modules

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,21 @@
 import type { NextPage } from 'next'
+import { GetStaticProps } from 'next'
+import { QueryClient, useQueryClient } from 'react-query'
+import { dehydrate } from 'react-query/hydration'
 import { Layout } from '../components/Layout'
 import { Auth } from '../components/Auth'
+import { fetchNews } from '../hooks/useQueryNews'
+import { News } from '../types/types'
+
+export const getStaticProps: GetStaticProps = async () => {
+  const queryClient = new QueryClient()
+  await queryClient.prefetchQuery('news', fetchNews)
+  return {
+    props: {
+      dehydrate: dehydrate(queryClient),
+    },
+  }
+}
 
 const Home: NextPage = () => {
   return (
